431a1f92a7f549bb0df81be1a1244b1a97dceb5e
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-04-14 11:17:47 +0530

    start sequence timer

--- lib/timer.test.ts
+++ lib/timer.test.ts
@@ -1,24 +1,68 @@
 import { test, expect, beforeEach, describe, vi } from 'vitest';
-import { mock, instance, capture } from 'ts-mockito';
-import { Unit } from './timer';
-import { CoreTimer, TimerEvent } from './types';
+import { mock, instance, capture, when } from 'ts-mockito';
+
+import { CoreTimer, Timer, TimerEvent } from './types';
+import { Sequence, Unit } from './timer';
 
 describe('Unit', () => {
-    let core: CoreTimer;
+    let inner: CoreTimer;
 
     beforeEach(() => {
-        core = mock<CoreTimer>()
+        inner = mock<CoreTimer>()
     })
 
-    test('starting timer', () => {
-        const unit = new Unit(10, instance(core))
+    test('calling inner timer', () => {
+        const unit = new Unit(10, instance(inner))
         const callback = vi.fn<[TimerEvent], void>()
         const e: TimerEvent = { type: 'tick' }
 
         unit.start(callback)
 
-        const [coreCallback] = capture(core.start).first()
+        const [coreCallback] = capture(inner.start).first()
         coreCallback(e)
         expect(callback).toHaveBeenCalledWith(e)
     })
 });
+
+describe('Sequence', () => {
+    let inner1: Timer;
+    let inner2: Timer;
+
+    beforeEach(() => {
+        inner1 = mock<Timer>();
+        inner2 = mock<Timer>();
+    })
+
+    test('duration is sum of inner durations', () => {
+        when(inner1.duration).thenReturn(10)
+        when(inner2.duration).thenReturn(20)
+
+        const seq = new Sequence([
+            instance(inner1),
+            instance(inner2)
+        ]);
+        expect(seq.duration).eq(30)
+    })
+
+    test('calling inner timers', () => {
+        const seq = new Sequence([
+            instance(inner1),
+            instance(inner2)
+        ]);
+        const callback = vi.fn<[TimerEvent], void>();
+
+        seq.start(callback);
+
+        const [inner1Callback] = capture(inner1.start).first()
+        inner1Callback({ type: 'tick' })
+        expect(callback).toHaveBeenNthCalledWith(1, { type: 'tick' })
+        inner1Callback({ type: 'done' })
+        expect(callback).toHaveBeenNthCalledWith(2, { type: 'tick' })
+
+        const [inner2Callback] = capture(inner2.start).first()
+        inner2Callback({ type: 'tick' })
+        expect(callback).toHaveBeenNthCalledWith(3, { type: 'tick' })
+        inner2Callback({ type: 'done' })
+        expect(callback).toHaveBeenNthCalledWith(4, { type: 'done' })
+    })
+})
--- lib/timer.ts
+++ lib/timer.ts
@@ -19,3 +19,33 @@ export class Unit implements Timer {
     stop() {}
 }
 
+export class Sequence implements Timer {
+    public readonly duration: number;
+    private readonly innerTimers: Timer[];
+    private current: number = 0;
+
+    constructor(innerTimers: Timer[]) {
+        this.innerTimers = innerTimers;
+        this.duration = this.innerTimers.reduce((totalDuration, t) => totalDuration + t.duration, 0);
+    }
+
+    start(callback: TimerEventHandler): void {
+        const t = this.innerTimers[this.current]
+        t.start(e => {
+            if (e.type === 'tick') callback(e)  // base case 1
+            if (e.type === 'done') {
+                if (this.current === this.innerTimers.length - 1) {
+                    callback(e)                 // base case 2
+                } else {
+                    callback({ type: 'tick' })
+                    this.current++
+                    this.start(callback)
+                }
+            }
+        })
+    }
+
+    pause() {}
+    stop() {}
+}
+
