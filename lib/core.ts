eacac2d49f2ed6d29b11c3078ff2f3a2851b3082
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-04-19 12:57:54 +0530

    core: use perf.now() and setTimeout instead of setInterval

--- lib/core.ts
+++ lib/core.ts
@@ -1,22 +1,39 @@
 import { TimeIt, TimerEventHandler } from "./types";
 
 export const webApiCountdown: TimeIt = (duration: number, onEvent: TimerEventHandler) => {
+    /**
+     * https://gist.github.com/jakearchibald/cb03f15670817001b1157e62a076fe95
+     */
+
+    const ms = 1000;
+    const start = performance.now();
     let timeoutId: ReturnType<typeof setInterval> | undefined;
     let elapsed = 0;
 
-    timeoutId = setInterval(() => {
+    function frame() {
         elapsed++;
         if (elapsed < duration) {
-            onEvent({ type: 'tick' })
+            onEvent({ type: 'tick' });
+            scheduleFrame(performance.now());
         } else {
             clearTimeout(timeoutId)
-            onEvent({ type: 'done' })
+            onEvent({ type: 'done' });
         }
-    }, 1000)
+    }
+
+    function scheduleFrame(now: number) {
+        const elapsed = now - start;
+        const elapsedRounded = Math.round(elapsed / ms) * ms;
+        const targetNext = start + elapsedRounded + ms;
+        const delay = targetNext - now;
+        timeoutId = setTimeout(() => frame(), delay);
+    }
+
+    scheduleFrame(start);
 
     return {
         stop() {
-            clearInterval(timeoutId);
+            clearTimeout(timeoutId);
         }
     }
 }
--- lib/main.ts
+++ lib/main.ts
@@ -1,1 +1,6 @@
-console.log('hello')
+import { webApiCountdown } from "./core";
+
+const start = performance.now()
+webApiCountdown(120, e => {
+    console.log([e.type, performance.now() - start])
+})
